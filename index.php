740531451496972c405b34d60437088775ca171c
Mickael Allonneau <mickael.allonneau@gmail.com>
2014-03-28 18:01:23 +0100

    Main page now redirects to /HotlinkJS/

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Location: HotlinkJS');
+exit();
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>flawyte's GitHub</title>
+    </head>
+    <body>
+        <p>Hello GitHub.io !</p>
+    </body>
+</html>
