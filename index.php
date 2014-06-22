396b8f291d0f59fbef0ffa5fecd7c61662a44498
Mickael Allonneau <mickael.allonneau@gmail.com>
2014-06-22 11:41:40 +0200

    index> Added link to 'HotlinkJS' sub-page

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
-<?php
-
-header('Location: HotlinkJS');
-exit();
-
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
     <head>
         <title>flawyte's GitHub</title>
     </head>
     <body>
-        <p>Hello GitHub.io !</p>
+        <a href="https://flawyte.github.io/HotlinkJS">HotlinkJS</a>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;Get Dropbox and Google Drive hotlinks for you image files.</p>
     </body>
 </html>
