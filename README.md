8f9d264deb389e3ec17612e8c064cd673446cb4e
Mickaël Allonneau <mickael.allonneau@gmail.com>
2018-10-22 09:58:21 +0200

    Hard reset

--- README.md
+++
@@ -1,2 +0,0 @@
-flawyte.github.io
-=================
--- models/project.js
+++
@@ -1,5 +0,0 @@
-function Project(properties) {
-  this.description = properties.description || null;
-  this.name = properties.name || null;
-  this.url = properties.url || null;
-}
